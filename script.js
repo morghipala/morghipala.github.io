9340690f3509056026e83251fc3721f2a4b48413
morghipala <morghipalazzini@gmail.com>
2024-03-17 22:45:16 +0100

    update

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
+// Funzione per caricare i file HTML dalla directory "blocks" in base all'ordine definito in order.json
 function loadHTMLFilesInOrder() {
-
+    // Percorso della directory "blocks"
     var htmlDirectoryPath = "blocks/";
 
+    // Carica l'ordine definito in order.json
     fetch("order.json")
         .then(response => response.json())
         .then(data => {
             var order = data.order;
-            var loadedCount = 0; 
 
-            order.forEach(function (fileName) {
-                var fullFileName = fileName + ".html"; 
-                fetch(htmlDirectoryPath + fullFileName)
+            // Funzione per caricare un singolo file HTML
+            function loadHTMLFile(fileName) {
+                var fullFileName = fileName + ".html"; // Aggiungi l'estensione .html
+                return fetch(htmlDirectoryPath + fullFileName)
                     .then(response => {
                         if (response.ok) {
                             return response.text();
@@ -20,36 +22,41 @@ function loadHTMLFilesInOrder() {
                         }
                     })
                     .then(data => {
-
+                        // Creare un nuovo elemento <div> per contenere il contenuto HTML
                         var div = document.createElement("div");
                         div.innerHTML = data;
 
+                        // Inserire il contenuto HTML nel div "thumbnails"
                         var thumbnailsDiv = document.getElementById("thumbnails");
                         thumbnailsDiv.appendChild(div);
-
-                        loadedCount++;
-
-                        if (loadedCount === order.length) {
-                            console.log("Tutti i file HTML sono stati caricati.");
-                        }
                     })
                     .catch(error => console.error('Error loading HTML file:', error));
-            });
+            }
+
+            // Carica i file HTML in base all'ordine definito
+            var loadPromises = order.map(loadHTMLFile);
+            Promise.all(loadPromises)
+                .then(() => {
+                    console.log("Tutti i file HTML sono stati caricati nell'ordine definito.");
+                });
         })
         .catch(error => console.error('Error loading order.json:', error));
 }
 
+// Funzione per caricare i file CSS dalla directory "css"
 function loadCSSFiles() {
-
+    // Percorso della directory "css"
     var cssDirectoryPath = "css/";
 
+    // Carica i file CSS definiti in cssfiles.json
     fetch("cssfiles.json")
         .then(response => response.json())
         .then(data => {
             var cssFiles = data.files;
 
+            // Caricare ciascun file CSS e aggiungerlo all'head della pagina HTML
             cssFiles.forEach(function (fileName) {
-                var fullFileName = fileName + ".css"; 
+                var fullFileName = fileName + ".css"; // Aggiungi l'estensione .css
                 var link = document.createElement("link");
                 link.rel = "stylesheet";
                 link.type = "text/css";
@@ -60,7 +67,22 @@ function loadCSSFiles() {
         .catch(error => console.error('Error loading cssfiles.json:', error));
 }
 
+// Chiamare le funzioni al caricamento della pagina
 window.onload = function () {
     loadCSSFiles();
     loadHTMLFilesInOrder();
 };
+
+
+let magicGrid = new MagicGrid({
+    container: "#thumbnails",
+    static: false,
+    items: 11,
+    gutter: 15,
+    maxColumns: 3,
+    useTransform: true,
+    animate: true,
+});
+
+magicGrid.listen();
+
