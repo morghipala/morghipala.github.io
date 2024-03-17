c546d1992cf0beb490af43ee097036f14ce08cf6
morghipala <morghipalazzini@gmail.com>
2024-03-17 17:18:59 +0100

    update

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// Funzione per caricare i file HTML dalla directory "blocks" in base all'ordine definito in order.json
 function loadHTMLFilesInOrder() {
-
+    // Percorso della directory "blocks"
     var htmlDirectoryPath = "blocks/";
 
+    // Carica l'ordine definito in order.json
     fetch("order.json")
         .then(response => response.json())
         .then(data => {
             var order = data.order;
 
+            // Funzione per caricare un singolo file HTML
             function loadFile(fileName) {
-                var fullFileName = fileName + ".html"; 
+                var fullFileName = fileName + ".html"; // Aggiungi l'estensione .html
                 fetch(htmlDirectoryPath + fullFileName)
                     .then(response => {
                         if (response.ok) {
@@ -19,66 +22,75 @@ function loadHTMLFilesInOrder() {
                         }
                     })
                     .then(data => {
-
+                        // Creare un nuovo elemento <div> per contenere il contenuto HTML
                         var div = document.createElement("div");
                         div.innerHTML = data;
 
+                        // Inserire il contenuto HTML nel div "thumbnails"
                         var thumbnailsDiv = document.getElementById("thumbnails");
                         thumbnailsDiv.appendChild(div);
                     })
                     .catch(error => console.error('Error loading HTML file:', error));
             }
 
+            // Carica i file HTML in base all'ordine definito
             order.forEach(function (fileName) {
                 loadFile(fileName);
             });
 
+            // Carica i file HTML rimanenti nella directory che non sono definiti in order.json
             fetch(htmlDirectoryPath)
                 .then(response => response.text())
                 .then(data => {
-
+                    // Creare un oggetto DOM temporaneo per analizzare il contenuto della directory
                     var parser = new DOMParser();
                     var xmlDoc = parser.parseFromString(data, "text/html");
 
+                    // Ottenere una lista di tutti i link ai file nella directory
                     var links = xmlDoc.getElementsByTagName("a");
                     var htmlFiles = [];
                     for (var i = 0; i < links.length; i++) {
                         var href = links[i].getAttribute("href");
-
-                        if (href.endsWith(".html") && !order.includes(href.slice(0, -5))) { 
+                        // Aggiungere solo i file HTML alla lista che non sono presenti in order.json
+                        if (href.endsWith(".html") && !order.includes(href.slice(0, -5))) { // Rimuovi l'estensione .html
                             htmlFiles.push(href);
                         }
                     }
 
+                    // Caricare ciascun file HTML rimanente e aggiungerlo alla fine
                     htmlFiles.forEach(function (file) {
-                        loadFile(file.slice(0, -5)); 
+                        loadFile(file.slice(0, -5)); // Rimuovi l'estensione .html
                     });
                 });
         })
         .catch(error => console.error('Error loading order.json:', error));
 }
 
+// Funzione per caricare i file CSS dalla directory "css"
 function loadCSSFiles() {
-
+    // Percorso della directory "css"
     var cssDirectoryPath = "css/";
 
+    // Ottenere una lista dei file CSS dalla directory
     fetch(cssDirectoryPath)
         .then(response => response.text())
         .then(data => {
-
+            // Creare un oggetto DOM temporaneo per analizzare il contenuto della directory
             var parser = new DOMParser();
             var xmlDoc = parser.parseFromString(data, "text/html");
 
+            // Ottenere una lista di tutti i link ai file CSS nella directory
             var links = xmlDoc.getElementsByTagName("a");
             var cssFiles = [];
             for (var i = 0; i < links.length; i++) {
                 var href = links[i].getAttribute("href");
-
+                // Aggiungere solo i file CSS alla lista
                 if (href.endsWith(".css")) {
                     cssFiles.push(href);
                 }
             }
 
+            // Caricare ciascun file CSS e aggiungerlo all'head della pagina HTML
             cssFiles.forEach(function (file) {
                 var link = document.createElement("link");
                 link.rel = "stylesheet";
@@ -90,6 +102,7 @@ function loadCSSFiles() {
         .catch(error => console.error('Error loading CSS files:', error));
 }
 
+// Chiamare le funzioni al caricamento della pagina
 window.onload = function () {
     loadCSSFiles();
     loadHTMLFilesInOrder();
